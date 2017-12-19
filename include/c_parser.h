daa55b53500a748f4a48de0fbbf6fcfa05e26abe
shiftdj <shiftdj.suburb@yahoo.com>
2017-12-19 22:41:44 +0300

    Incapsulation of c-parser

--- include/c_parser.h
+++ include/c_parser.h
@@ -1,8 +1,12 @@
 #pragma once
+#include <iostream>
 #include <string>
+#include <regex>
+#include <unordered_set>
 #include <functional>
-#include "parse_tree.h"
-#include "c_token.h"
+#include "lalr_parser.h"
+#include "lexer.h"
+
 
 template <typename T>
 using function_t = std::function<bool(parse_tree<T>&)>;
@@ -10,3 +14,24 @@ using function_t = std::function<bool(parse_tree<T>&)>;
 extern const std::string c_parser_strings[];
 extern const uint64_t c_parser_size;
 extern const unsigned char c_parser_table[];
+
+enum c_token;
+
+class c_parser {
+	std::unordered_set<std::string> _typedefs;
+	std::unordered_set<std::string> _enums;
+	function_t<c_token> c_parser_functions[2];
+	lalr_table<c_token> _table;
+	lalr_parser<c_token, std::string> _parser;
+	lexer _lexer;
+public:
+	c_parser();
+	c_parser(std::istream& o);
+	void set_stream(std::istream& o);
+	bool eof();
+	void next();
+	void run();
+	parse_tree<c_token>& get_tree();
+};
+
+
--- sources/c_parser.cpp
+++ sources/c_parser.cpp
@@ -1,4 +1,311 @@
 #include "c_parser.h"
+
+
+enum c_token {
+	t_empty,
+	t_auto,
+	t_break,
+	t_case,
+	t_char,
+	t_const,
+	t_continue,
+	t_default,
+	t_do,
+	t_double,
+	t_else,
+	t_enum,
+	t_extern,
+	t_float,
+	t_for,
+	t_goto,
+	t_if,
+	t_inline,
+	t_int,
+	t_long,
+	t_register,
+	t_restrict,
+	t_return,
+	t_short,
+	t_signed,
+	t_sizeof,
+	t_static,
+	t_struct,
+	t_switch,
+	t_typedef,
+	t_union,
+	t_unsigned,
+	t_void,
+	t_volatile,
+	t_while,
+	t_alignas,
+	t_alignof,
+	t_atomic,
+	t_bool,
+	t_complex,
+	t_generic,
+	t_imaginary,
+	t_noreturn,
+	t_static_assert,
+	t_thread_local,
+	t_func_name,
+	t_identifier,
+	t_i_constant,
+	t_f_constant,
+	t_string_literal,
+	t_ellipsis,
+	t_right_assign,
+	t_left_assign,
+	t_add_assign,
+	t_sub_assign,
+	t_mul_assign,
+	t_div_assign,
+	t_mod_assign,
+	t_and_assign,
+	t_xor_assign,
+	t_or_assign,
+	t_right_op,
+	t_left_op,
+	t_inc_op,
+	t_dec_op,
+	t_ptr_op,
+	t_and_op,
+	t_or_op,
+	t_le_op,
+	t_ge_op,
+	t_eq_op,
+	t_ne_op,
+	t_semicolon,
+	t_brace_left,
+	t_brace_right,
+	t_comma,
+	t_colon,
+	t_assign,
+	t_round_left,
+	t_round_right,
+	t_square_left,
+	t_square_right,
+	t_dot,
+	t_ampersand,
+	t_exmark,
+	t_tilda,
+	t_minus,
+	t_plus,
+	t_star,
+	t_slash,
+	t_percent,
+	t_angle_left,
+	t_angle_right,
+	t_carrete,
+	t_vertical,
+	t_qmark,
+	t_typedef_name,
+	t_enumeration_constant
+};
+
+c_parser::c_parser() :
+	c_parser_functions{
+		[this](parse_tree<c_token>& data) mutable {
+			_enums.insert(data.last().at(0).data());
+			return true;
+		} ,
+
+		[this](parse_tree<c_token>& data) mutable {
+			parse_tree<c_token>& current = data.last();
+			if (current.at(0).begin()->id() == t_typedef) {
+				for (size_t i = 0; i < current.at(1).size(); ++i)
+					for (auto iter = current.at(1).at(i).begin(); iter != current.at(1).at(i).end(); ++iter) {
+						if (iter->id() == t_identifier) {
+							_typedefs.insert(iter->data());
+							break;
+						}
+					}
+			}
+			return true;
+		}
+	},
+	_table(c_parser_table, c_parser_size, c_parser_strings, c_parser_functions),
+	_parser(_table),
+	_lexer(" \t\n", "'\"",
+	{
+		{ "//", "\n" },
+		{ "/*", "*/" }
+	},
+	true,
+	{
+		"...", ">>=", "<<=", "+=", "-=", "*=", "/=", "%=", "&=", "^=", "|=", ">>", "<<", "++", "--", "->",
+		"&&", "||", "<=", ">=", "==", "!=", ";", "{", "<%", "}", "%>", ",", ":", "=", "(", ")",
+		"[", "<:", "]", ":>", ".", "&", "!", "~", "-", "+", "*", "/", "%", "<", ">", "^",
+		"|", "?"
+	}) {
+		//Lexems adapted from http://www.quut.com/c/ANSI-C-grammar-l-2011.html
+		std::string r_O("[0-7]");
+		std::string r_D("[0-9]");
+		std::string r_NZ("[1-9]");
+		std::string r_L("[a-zA-Z_]");
+		std::string r_A("[a-zA-Z_0-9]");
+		std::string r_H("[a-fA-F0-9]");
+		std::string r_HP("(0[xX])");
+		std::string r_E("([Ee][+-]?[0-9]+)");
+		std::string r_P("([Pp][+-]?[0-9]+)");
+		std::string r_FS("(f|F|l|L)");
+		std::string r_IS("(((u|U)(l|L|ll|LL)?)|((l|L|ll|LL)(u|U)?))");
+		std::string r_CP("(u|U|L)");
+		std::string r_SP("(u8|u|U|L)");
+		std::string r_ES("(\\\\(['\"\\?\\\\abfnrtv]|[0-7]{1,3}|x[a-fA-F0-9]+))");
+		std::string r_WS("[ \t\v\n\f]");
+
+		_lexer.add_keyword("auto", [](std::string& name) { return t_auto; });
+		_lexer.add_keyword("break", [](std::string& name) { return t_break; });
+		_lexer.add_keyword("case", [](std::string& name) { return t_case; });
+		_lexer.add_keyword("char", [](std::string& name) { return t_char; });
+		_lexer.add_keyword("const", [](std::string& name) { return t_const; });
+		_lexer.add_keyword("continue", [](std::string& name) { return t_continue; });
+		_lexer.add_keyword("default", [](std::string& name) { return t_default; });
+		_lexer.add_keyword("do", [](std::string& name) { return t_do; });
+		_lexer.add_keyword("double", [](std::string& name) { return t_double; });
+		_lexer.add_keyword("else", [](std::string& name) { return t_else; });
+		_lexer.add_keyword("enum", [](std::string& name) { return t_enum; });
+		_lexer.add_keyword("extern", [](std::string& name) { return t_extern; });
+		_lexer.add_keyword("float", [](std::string& name) { return t_float; });
+		_lexer.add_keyword("for", [](std::string& name) { return t_for; });
+		_lexer.add_keyword("goto", [](std::string& name) { return t_goto; });
+		_lexer.add_keyword("if", [](std::string& name) { return t_if; });
+		_lexer.add_keyword("inline", [](std::string& name) { return t_inline; });
+		_lexer.add_keyword("int", [](std::string& name) { return t_int; });
+		_lexer.add_keyword("long", [](std::string& name) { return t_long; });
+		_lexer.add_keyword("register", [](std::string& name) { return t_register; });
+		_lexer.add_keyword("restrict", [](std::string& name) { return t_restrict; });
+		_lexer.add_keyword("return", [](std::string& name) { return t_return; });
+		_lexer.add_keyword("short", [](std::string& name) { return t_short; });
+		_lexer.add_keyword("signed", [](std::string& name) { return t_signed; });
+		_lexer.add_keyword("sizeof", [](std::string& name) { return t_sizeof; });
+		_lexer.add_keyword("static", [](std::string& name) { return t_static; });
+		_lexer.add_keyword("struct", [](std::string& name) { return t_struct; });
+		_lexer.add_keyword("switch", [](std::string& name) { return t_switch; });
+		_lexer.add_keyword("typedef", [](std::string& name) { return t_typedef; });
+		_lexer.add_keyword("union", [](std::string& name) { return t_union; });
+		_lexer.add_keyword("unsigned", [](std::string& name) { return t_unsigned; });
+		_lexer.add_keyword("void", [](std::string& name) { return t_void; });
+		_lexer.add_keyword("volatile", [](std::string& name) { return t_volatile; });
+		_lexer.add_keyword("while", [](std::string& name) { return t_while; });
+		_lexer.add_keyword("_Alignas", [](std::string& name) { return t_alignas; });
+		_lexer.add_keyword("_Alignof", [](std::string& name) { return t_alignof; });
+		_lexer.add_keyword("_Atomic", [](std::string& name) { return t_atomic; });
+		_lexer.add_keyword("_Bool", [](std::string& name) { return t_bool; });
+		_lexer.add_keyword("_Complex", [](std::string& name) { return t_complex; });
+		_lexer.add_keyword("_Generic", [](std::string& name) { return t_generic; });
+		_lexer.add_keyword("_Imaginary", [](std::string& name) { return t_imaginary; });
+		_lexer.add_keyword("_Noreturn", [](std::string& name) { return t_noreturn; });
+		_lexer.add_keyword("_Static_assert", [](std::string& name) { return t_static_assert; });
+		_lexer.add_keyword("_Thread_local", [](std::string& name) { return t_thread_local; });
+		_lexer.add_keyword("__func__", [](std::string& name) { return t_func_name; });
+
+		_lexer.add_regular(std::regex(r_L + r_A + "*"), [this](std::string& name) {
+			if (_typedefs.find(name) != _typedefs.end())
+				return t_typedef_name;
+			if (_enums.find(name) != _enums.end())
+				return t_enumeration_constant;
+			return t_identifier;
+		});
+
+		_lexer.add_regular(std::regex(r_HP + r_H + "+" + r_IS + "?"), [](std::string& name) { return t_i_constant; });
+		_lexer.add_regular(std::regex(r_NZ + r_D + "*" + r_IS + "?"), [](std::string& name) { return t_i_constant; });
+		_lexer.add_regular(std::regex("0" + r_O + "*" + r_IS + "?"), [](std::string& name) { return t_i_constant; });
+		_lexer.add_regular(std::regex(r_CP + "?'([^'\\\n]|" + r_ES + ")+'"), [](std::string& name) { return t_i_constant; });
+
+		_lexer.add_regular(std::regex(r_D + "+" + r_E + r_FS + "?"), [](std::string& name) { return t_f_constant; });
+		_lexer.add_regular(std::regex(r_D + "*\\." + r_D + "+" + r_E + "?" + r_FS + "?"), [](std::string& name) { return t_f_constant; });
+		_lexer.add_regular(std::regex(r_D + "+\\." + r_E + "?" + r_FS + "?"), [](std::string& name) { return t_f_constant; });
+		_lexer.add_regular(std::regex(r_D + "+\\." + r_E + "?" + r_FS + "?"), [](std::string& name) { return t_f_constant; });
+		_lexer.add_regular(std::regex(r_HP + r_H + "+" + r_P + r_FS + "?"), [](std::string& name) { return t_f_constant; });
+		_lexer.add_regular(std::regex(r_HP + r_H + "*\\." + r_H + "+" + r_P + r_FS + "?"), [](std::string& name) { return t_f_constant; });
+		_lexer.add_regular(std::regex(r_HP + r_H + "+\\." + r_P + r_FS + "?"), [](std::string& name) { return t_f_constant; });
+
+		_lexer.add_regular(std::regex("(" + r_SP + "?\\\"([^\"\\\n]|" + r_ES + ")*\\\"" + r_WS + "*)+"), [](std::string& name) { return t_string_literal; });
+
+		_lexer.add_keyword("...", [](std::string& name) { return t_ellipsis; });
+		_lexer.add_keyword(">>=", [](std::string& name) { return t_right_assign; });
+		_lexer.add_keyword("<<=", [](std::string& name) { return t_left_assign; });
+		_lexer.add_keyword("+=", [](std::string& name) { return t_add_assign; });
+		_lexer.add_keyword("-=", [](std::string& name) { return t_sub_assign; });
+		_lexer.add_keyword("*=", [](std::string& name) { return t_mul_assign; });
+		_lexer.add_keyword("/=", [](std::string& name) { return t_div_assign; });
+		_lexer.add_keyword("%=", [](std::string& name) { return t_mod_assign; });
+		_lexer.add_keyword("&=", [](std::string& name) { return t_and_assign; });
+		_lexer.add_keyword("^=", [](std::string& name) { return t_xor_assign; });
+		_lexer.add_keyword("|=", [](std::string& name) { return t_or_assign; });
+		_lexer.add_keyword(">>", [](std::string& name) { return t_right_op; });
+		_lexer.add_keyword("<<", [](std::string& name) { return t_left_op; });
+		_lexer.add_keyword("++", [](std::string& name) { return t_inc_op; });
+		_lexer.add_keyword("--", [](std::string& name) { return t_dec_op; });
+		_lexer.add_keyword("->", [](std::string& name) { return t_ptr_op; });
+		_lexer.add_keyword("&&", [](std::string& name) { return t_and_op; });
+		_lexer.add_keyword("||", [](std::string& name) { return t_or_op; });
+		_lexer.add_keyword("<=", [](std::string& name) { return t_le_op; });
+		_lexer.add_keyword(">=", [](std::string& name) { return t_ge_op; });
+		_lexer.add_keyword("==", [](std::string& name) { return t_eq_op; });
+		_lexer.add_keyword("!=", [](std::string& name) { return t_ne_op; });
+		_lexer.add_keyword(";", [](std::string& name) { return t_semicolon; });
+		_lexer.add_keyword("{", [](std::string& name) { return t_brace_left; });
+		_lexer.add_keyword("<%", [](std::string& name) { return t_brace_left; });
+		_lexer.add_keyword("}", [](std::string& name) { return t_brace_right; });
+		_lexer.add_keyword("%>", [](std::string& name) { return t_brace_right; });
+		_lexer.add_keyword(",", [](std::string& name) { return t_comma; });
+		_lexer.add_keyword(":", [](std::string& name) { return t_colon; });
+		_lexer.add_keyword("=", [](std::string& name) { return t_assign; });
+		_lexer.add_keyword("(", [](std::string& name) { return t_round_left; });
+		_lexer.add_keyword(")", [](std::string& name) { return t_round_right; });
+		_lexer.add_keyword("[", [](std::string& name) { return t_square_left; });
+		_lexer.add_keyword("<:", [](std::string& name) { return t_square_left; });
+		_lexer.add_keyword("]", [](std::string& name) { return t_square_right; });
+		_lexer.add_keyword(":>", [](std::string& name) { return t_square_right; });
+		_lexer.add_keyword(".", [](std::string& name) { return t_dot; });
+		_lexer.add_keyword("&", [](std::string& name) { return t_ampersand; });
+		_lexer.add_keyword("!", [](std::string& name) { return t_exmark; });
+		_lexer.add_keyword("~", [](std::string& name) { return t_tilda; });
+		_lexer.add_keyword("-", [](std::string& name) { return t_minus; });
+		_lexer.add_keyword("+", [](std::string& name) { return t_plus; });
+		_lexer.add_keyword("*", [](std::string& name) { return t_star; });
+		_lexer.add_keyword("/", [](std::string& name) { return t_slash; });
+		_lexer.add_keyword("%", [](std::string& name) { return t_percent; });
+		_lexer.add_keyword("<", [](std::string& name) { return t_angle_left; });
+		_lexer.add_keyword(">", [](std::string& name) { return t_angle_right; });
+		_lexer.add_keyword("^", [](std::string& name) { return t_carrete; });
+		_lexer.add_keyword("|", [](std::string& name) { return t_vertical; });
+		_lexer.add_keyword("?", [](std::string& name) { return t_qmark; });
+
+		//_lexer.add_keyword("\n", [](std::string& name) { return 0; });
+		//_lexer.add_keyword(" ", [](std::string& name) { return 0; });
+}
+
+
+c_parser::c_parser(std::istream& o) : c_parser() {
+	set_stream(o);
+}
+
+void c_parser::set_stream(std::istream& o) {
+	_lexer.set_stream(o);
+}
+
+bool c_parser::eof() {
+	return _lexer.eof();
+}
+void c_parser::next() {
+	token t = _lexer.next(); //TO-DO: template lexer
+	_parser.next((c_token)t.index(), t.data());
+}
+void  c_parser::run() {
+	while (!eof())
+		next();
+}
+
+parse_tree<c_token>& c_parser::get_tree() {
+	return _parser.get_tree();
+}
+
+
+
+
 const std::string c_parser_strings[] = { "storage_class_specifier", "type_specifier", "type_qualifier", "function_specifier", "struct_or_union", "translation_unit", "external_declaration", "declaration_specifiers", "declaration", "enum_specifier", "pointer", "direct_declarator", "init_declarator", "init_declarator_list", "declarator", "struct_or_union_specifier", "unary_operator", "primary_expression", "constant", "string", "constant_expression", "conditional_expression", "logical_and_expression", "logical_or_expression", "exclusive_or_expression", "inclusive_or_expression", "equality_expression", "and_expression", "relational_expression", "shift_expression", "additive_expression", "multiplicative_expression", "cast_expression", "unary_expression", "postfix_expression", "type_name", "specifier_qualifier_list", "enumeration_constant", "enumerator_list", "enumerator", "type_qualifier_list", "function_definition", "declaration_list", "struct_declaration_list", "struct_declaration", "expression", "assignment_expression", "atomic_type_specifier", "abstract_declarator", "alignment_specifier", "initializer", "compound_statement", "expression_statement", "block_item", "block_item_list", "statement", "identifier_list", "parameter_list", "parameter_type_list", "parameter_declaration", "struct_declarator_list", "struct_declarator", "assignment_operator", "argument_expression_list", "direct_abstract_declarator", "initializer_list", "designator_list", "jump_statement", "designator", "designation", "labeled_statement", "generic_assoc_list", "static_assert_declaration", "generic_selection", "selection_statement", "iteration_statement", "generic_association" };
 const uint64_t c_parser_size = 12272;
 const unsigned char c_parser_table[] = {
